4ce8d6415bd510654a3ce78c1d16af29edc157c0
kronos-27 <simonyi.patrik@gmail.com>
2020-06-05 13:07:28 +0200

    Updates

---
+++ precache-manifest.c2d2e4e12327cf644162cf2c4b5f8e43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe0d50820ba5dc2d6f6eac6eabbfe00",
+    "url": "/statego-app/index.html"
+  },
+  {
+    "revision": "7a0816df1168cd5074de",
+    "url": "/statego-app/static/css/main.d3ae0000.chunk.css"
+  },
+  {
+    "revision": "d803b66380b54beda627",
+    "url": "/statego-app/static/js/2.ac8bcbef.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/statego-app/static/js/2.ac8bcbef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a0816df1168cd5074de",
+    "url": "/statego-app/static/js/main.a15f9e19.chunk.js"
+  },
+  {
+    "revision": "b4a38de8ff0a4752ba49",
+    "url": "/statego-app/static/js/runtime-main.af71bc4a.js"
+  }
+]);
